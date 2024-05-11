1549df4d91ed4f1dbd04e1d4687ba9696ae94e94
NetaCohen4 <netaco432@gmail.com>
2024-05-11 21:24:55 +0300

    commit

---
+++ Graph.cpp
@@ -0,0 +1,16 @@
+/*
+    325195774
+    netaco432@gmail.com
+*/
+
+class Graph {
+    int n; // n is number of vertices in the graph
+    bool **arcs; // n*n
+    int **e_weight; // in use only if the graph is weighted
+    bool isDirected;
+    bool isWeighted;
+
+
+    void loadGraph(){};
+    void printGraph(){};
+};
